38774c5b69d4bf2770c2eaa0765757694d13114d
mikedawn <biggrayson_2000@yahoo.com>
2019-10-28 21:25:44 -0500

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./utils/APIs";
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ client/src/utils/APIs.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export default {
+  // Searches the NYT books according to the search criteria given in the form
+  getNewBooks: function(title) {
+    return axios.get("https://www.googleapis.com/books/v1/volumes?q=pride+prejudice&download=epub&key=AIzaSyDTV3XSsYEPP_poJyQwcac1NIDsD6Y-DBA" + title);
+  },
+
+  //API Key AIzaSyDTV3XSsYEPP_poJyQwcac1NIDsD6Y-DBA
+  // Gets saved Books
+  getSavedBooks: function() {
+    return axios.get("/api/books/");
+  },
+  // Deletes the Book with the given id
+  deleteBook: function(id) {
+    return axios.delete("/api/books/" + id);
+  },
+  // Saves a Book to the database
+  saveBook: function(bookData) {
+    return axios.post("/api/books", bookData);
+  }
+};
